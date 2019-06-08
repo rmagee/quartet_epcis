aba4c0b26230d4a2f0d2ac280771ced2f139f215
Serial Lab <slab@serial-lab.com>
2019-06-08 11:01:49 -0600

    updated README to compile to RST

--- setup.py
+++ setup.py
@@ -38,13 +38,12 @@ if sys.argv[-1] == 'tag':
     sys.exit()
 
 readme = open('README.rst').read()
-history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='quartet_epcis',
     version=version,
     description="""Defines EPCIS models and XML parsing.""",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     author='SerialLab Corp',
     author_email='slab@serial-lab.com',
     url='https://gitlab.com/serial-lab/quartet_epcis',
